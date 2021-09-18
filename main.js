fbc06d65922bce2810aacfa8ef053a546dfd89b2
Maliksalmon2008 <83311416+Maliksalmon2008@users.noreply.github.com>
2021-09-18 12:49:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function login(){
+    player_one = document.getElementById("player1_name_input").value;
+    player_two = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1",player_one);
+    localStorage.setItem("player2",player_two);
+    window.location ="game_page.html";
+    }
